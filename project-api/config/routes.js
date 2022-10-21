53e3b026857df654d3050d9e20d951463f1a1f45
jamesjoel <jamesjoelj@gmail.com>
2022-10-21 14:19:37 +0530

    api

---
+++ project-api/config/routes.js
@@ -0,0 +1,8 @@
+const routes = require("express").Router();
+
+routes.use("/api/city", require("../controllers/CityController"));
+routes.use("/api/category", require("../controllers/CategoryController"));
+routes.use("/api/product", require("../controllers/ProductController"));
+
+
+module.exports = routes;
